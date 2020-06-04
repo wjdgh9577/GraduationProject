530cc36a54d7c0010dab3cab5a424c8d633b134d
Jungho Lee <wjdgh9577@naver.com>
2020-06-04 19:38:50 +0900

    2020.6.4

--- bvhparsing.py
+++ bvhparsing.py
@@ -77,7 +77,7 @@ def draw(tree, frame_number, joint='ROOT', parentPos=None, color=[0, 1, 0]):
 
         draw(tree, frame_number, child.name, childPos, C)
 
-with open("Male2_bvh/Male2_A1_Stand.bvh") as f:
+with open("Male2_bvh/example1.bvh") as f:
     mocap = Bvh(f.read())
 
 joints, tree = scan(mocap)
